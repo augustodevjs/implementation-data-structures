3aa4b05b57b6c42095a79fd1cc1c1e91333200da
augusto <augustosaaude@gmail.com>
2024-06-22 13:49:45 -0300

    feat: add stack implemented with array

---
+++ part-01/StackUsingArray/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace StackUsingArray;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        var stack = new Stack<int>();
+
+        stack.Push(10);
+        stack.Push(20);
+        stack.Push(30);
+        stack.Push(40);
+        stack.Push(50);
+        stack.Push(60);
+
+        Console.WriteLine(stack.Pop());
+        Console.WriteLine(stack.Pop());
+
+        Console.WriteLine("A pilha está vazia? " + stack.IsEmpty());
+
+        stack.Push(70);
+        stack.Push(80);
+
+        Console.WriteLine(stack.Pop());
+        Console.WriteLine(stack.Pop());
+        Console.WriteLine(stack.Pop());
+        Console.WriteLine(stack.Pop());
+        Console.WriteLine(stack.Pop());
+        Console.WriteLine(stack.Pop());
+
+        Console.WriteLine("A pilha está vazia? " + stack.IsEmpty());
+    }
+}
---
+++ part-01/StackUsingArray/Stack.cs
@@ -0,0 +1,52 @@
+﻿namespace StackUsingArray;
+
+public class Stack<T>
+{
+    private T[] stack;
+    private int top = 0;
+    private const int DefaultCapacity = 5;
+
+    public Stack(int initialCapacity = DefaultCapacity)
+    {
+        stack = new T[initialCapacity];
+    }
+
+    public bool IsEmpty()
+    {
+        return top == 0;
+    }
+
+    public void Push(T element)
+    {
+        if (top == stack.Length)
+        {
+            T[] newStack = new T[stack.Length * 2];
+
+            for (int i = 0; i < stack.Length; i++)
+            {
+                newStack[i] = stack[i];
+            }
+
+            stack = newStack;
+        }
+
+        stack[top] = element;
+        top++;
+    }
+
+    public T Pop()
+    {
+        if (IsEmpty())
+        {
+            throw new InvalidOperationException("Stack Underflow");
+        }
+        else
+        {
+            top--;
+            T element = stack[top];
+            stack[top] = default;
+
+            return element;
+        }
+    }
+}
